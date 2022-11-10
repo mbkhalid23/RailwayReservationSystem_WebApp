296cf48c59361f16ea6cd9886da2020486c0417c
mbkhalid23 <mbkhalid23@gmail.com>
2022-11-10 13:46:19 +0500

    Add complete CRUD functionality for Schedules

--- RailwayReservationSystem/Areas/Admin/Controllers/ScheduleController.cs
+++ RailwayReservationSystem/Areas/Admin/Controllers/ScheduleController.cs
@@ -29,7 +29,7 @@ namespace RailwayReservationSystem.Areas.Admin.Controllers
         }
 
         //GET
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             ScheduleViewModel ScheduleView = new();
             {
@@ -50,19 +50,36 @@ namespace RailwayReservationSystem.Areas.Admin.Controllers
                     });
             };
 
+            if (id == null || id == 0)
+            {
+                //create a new schedule entry
                 return View(ScheduleView);
-        }
+            }
 
+            else
+            {
+                //update existing schedule entry
+                ScheduleView.Schedule = _unitOfWork.Schedule.GetFirstOrDefault(s => s.ScheduleId == id);
+
+                if (ScheduleView.Schedule == null)
+                {
+                    return NotFound();
+                }
+
+                return View(ScheduleView);
+
+            }
+        }
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(ScheduleViewModel ScheduleView)
+        public IActionResult Upsert(ScheduleViewModel ScheduleView)
         {
             if (ModelState.IsValid)
             {
-                if(ScheduleView.Schedule.SourceStationId == 0 || ScheduleView.Schedule.DestinationStationId == 0 ||ScheduleView.Schedule.TrainNo == 0)
+                if(ScheduleView.Schedule.SourceStationId == 0 || ScheduleView.Schedule.DestinationStationId == 0)
                 {
-                    TempData["error"] = "Source, Destination, and Train can't be empty";
+                    TempData["error"] = "Source and Destination can't be empty";
                     return View(ScheduleView);
                 }
 
@@ -71,122 +88,45 @@ namespace RailwayReservationSystem.Areas.Admin.Controllers
                     TempData["error"] = "Source and Destination can't be same";
                     return View(ScheduleView);
                 }
-                
-                ScheduleView.Schedule.Train = _unitOfWork.Train.GetFirstOrDefault(t => t.TrainNo == ScheduleView.Schedule.TrainNo);
 
-                if (ScheduleView.Schedule.Train.StationId != ScheduleView.Schedule.SourceStationId)
+                if(ScheduleView.Schedule.Arrival <= ScheduleView.Schedule.Departure)
                 {
-                    TempData["error"] = "This train is not available at the Source Station";
+                    TempData["error"] = "Arrival can't be before or same as Departure";
                     return View(ScheduleView);
                 }
 
+                if (ScheduleView.Schedule.TrainNo != null)
+                {
+                    ScheduleView.Schedule.Train = _unitOfWork.Train.GetFirstOrDefault(t => t.TrainNo == ScheduleView.Schedule.TrainNo);
+
+                    if (ScheduleView.Schedule.Train.StationId != ScheduleView.Schedule.SourceStationId)
+                    {
+                        TempData["error"] = "This train is not available at the Source Station";
+                        return View(ScheduleView);
+                    }
+                }
                 ScheduleView.Schedule.Journey = ScheduleView.Schedule.Arrival - ScheduleView.Schedule.Departure;
 
                 //Add the Schedule entry to database
-                _unitOfWork.Schedule.Add(ScheduleView.Schedule);
-                _unitOfWork.Save();
+                if (ScheduleView.Schedule.ScheduleId == 0)
+                {
+                    _unitOfWork.Schedule.Add(ScheduleView.Schedule);
+                    TempData["success"] = "Schedule entry added successfully";
+                }
 
-                TempData["success"] = "Schedule entry added successfully";
+                //Update an existing Schedule entry to database
+                else
+                {
+                    _unitOfWork.Schedule.Update(ScheduleView.Schedule);
+                    TempData["success"] = "Schedule entry updated successfully";
+                }
+
+                _unitOfWork.Save();
 
                 return RedirectToAction("Index");
             }
 
             return View(ScheduleView);
-        }
-
-        //GET
-        public IActionResult Update(int? id)
-        {
-            //Check if the id is null or 0
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            //Create a new ViewModel object to bind to the view
-            TrainsViewModel TrainsView = new();
-            {
-                //Get train
-                TrainsView.Train = _unitOfWork.Train.GetFirstOrDefault(t => t.TrainNo == id);
-
-                //Check if the train object is null
-                if (TrainsView.Train == null)
-                {
-                    return NotFound();
-                }
-
-                //Retrive a list of available stations for the dropdown
-                TrainsView.StationsList = _unitOfWork.Station.GetAll().Select(
-                    s => new SelectListItem
-                    {
-                        Text = s.City + ", " + s.Name,
-                        Value = s.StationId.ToString()
-                    }).OrderBy(x=> x.Text);
-
-                //Save the current station of train in the PrevStation variable
-                TrainsView.PrevStation = TrainsView.Train.StationId;
-
-            };
-
-            return View(TrainsView);
-        }
-
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Update(TrainsViewModel TrainsView)
-        {
-            //Check if the user accidently set the capacity less than the seats booked. We will lose the booking data otherwise
-            if (TrainsView.Train.Capacity < TrainsView.Train.SeatsBooked)
-            {
-                TempData["error"] = "Capacity cannot be less than seats booked";
-
-                return View(TrainsView);
-            }
-
-            if (ModelState.IsValid)
-            {
-                //Check if the train is moved to another station
-                if(TrainsView.Train.StationId != TrainsView.PrevStation)
-                {
-                    //Get new station
-                    TrainsView.Train.Station = _unitOfWork.Station.GetFirstOrDefault(s => s.StationId == TrainsView.Train.StationId);
-
-                    //Check if new station has space available for a train
-                    if(TrainsView.Train.Station.AvailableSlots <= 0)
-                    {
-                        TempData["error"] = "Station already at Max capacity. Can't move the Train";
-                        return View(TrainsView);
-                    }
-
-                    //Train arrives at the new station
-                    TrainsView.Train.Station.TrainsStationed++;
-                    TrainsView.Train.Station.AvailableSlots--;
-
-                    //Get prevoius station
-                    Station PreviousStation = _unitOfWork.Station.GetFirstOrDefault(s => s.StationId == TrainsView.PrevStation);
-
-                    //Train leaves the previous station
-                    PreviousStation.TrainsStationed--;
-                    PreviousStation.AvailableSlots++;
-
-                    //Update the previous station in database
-                    _unitOfWork.Station.Update(PreviousStation);
-                }
-
-                //Update Available Seats in the train
-                TrainsView.Train.SeatsAvailable = TrainsView.Train.Capacity - TrainsView.Train.SeatsBooked;
-
-                //Update the train in the database
-                _unitOfWork.Train.Update(TrainsView.Train);
-                _unitOfWork.Save();
-
-                TempData["success"] = "Train updated successfully";
-
-                return RedirectToAction("Index");
-            }
-
-            return View(TrainsView);
         }
 
         //GET
@@ -198,16 +138,16 @@ namespace RailwayReservationSystem.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            //Get train along with its station
-            Train obj = _unitOfWork.Train.GetFirstOrDefault(t => t.TrainNo == id, IncludeProperties:"Station");
+            //Get schedule along with its source, destination, and train details
+            Schedule schedule = _unitOfWork.Schedule.GetFirstOrDefault(s => s.ScheduleId == id, IncludeProperties: "Train,Source,Destination");
 
-            //Check if the train object is null
-            if (obj == null)
+            //Check if the schedule object is null
+            if (schedule == null)
             {
                 return NotFound();
             }
 
-            return View(obj);
+            return View(schedule);
         }
 
         //POST
@@ -221,32 +161,20 @@ namespace RailwayReservationSystem.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            //Get train along with its station
-            Train obj = _unitOfWork.Train.GetFirstOrDefault(t => t.TrainNo == id, IncludeProperties: "Station"); ;
+            //Get schedule along with its source, destination, and train details
+            Schedule schedule = _unitOfWork.Schedule.GetFirstOrDefault(s => s.ScheduleId == id);
 
-            //Check if the train object is null
-            if (obj == null)
+            //Check if the schedule object is null
+            if (schedule == null)
             {
                 return NotFound();
             }
 
-            //Check if there are any seats booked in the train. If so, cancel or reschedule all the bookings first
-            if (obj.SeatsBooked != 0)
-            {
-                TempData["error"] = "Cannot delete train: it has some seats booked";
-
-                return View(obj);
-            }
-
-            //Train leaves the station
-            obj.Station.TrainsStationed--;
-            obj.Station.AvailableSlots++;
-
-            //Remove the train from database
-            _unitOfWork.Train.Remove(obj);
+            //Remove the schedule entry from database
+            _unitOfWork.Schedule.Remove(schedule);
             _unitOfWork.Save();
 
-            TempData["success"] = "Train deleted successfully";
+            TempData["success"] = "Schedule entry deleted successfully";
 
             return RedirectToAction("Index");
         }
