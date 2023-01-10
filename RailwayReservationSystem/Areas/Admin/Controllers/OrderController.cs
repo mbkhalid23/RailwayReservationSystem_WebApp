040ebf548954e306702248b792cb0490d98f0bee
mbkhalid23 <mbkhalid23@gmail.com>
2023-01-10 21:44:51 +0500

    Add orders list page

---
+++ RailwayReservationSystem/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RailwayReservationSystem.DataAccess.Repository.IRepository;
+using RailwayReservationSystem.Models;
+using RailwayReservationSystem.Utility;
+using System.Security.Claims;
+
+namespace RailwayReservationSystem.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			List<OrderHeader> orderHeaders;
+
+			//If the user is Admin then Get all orders. Else, since the user will be customer, get only order of that specific customer
+			if (User.IsInRole(SD.Role_Admin))
+			{
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(IncludeProperties: "ApplicationUser").ToList();
+            }
+			else
+			{
+				//Get the application user from claims identity
+				var claimsIdentity = (ClaimsIdentity)User.Identity;
+				var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+				//Get the orders for current user
+				orderHeaders = _unitOfWork.OrderHeader.GetAll(x => x.ApplicationUserId == claim.Value, IncludeProperties: "ApplicationUser").ToList();
+			}
+
+            return View(orderHeaders);
+		}
+	}
+}
