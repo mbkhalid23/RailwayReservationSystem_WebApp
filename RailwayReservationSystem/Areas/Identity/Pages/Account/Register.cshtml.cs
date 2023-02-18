b48060eef8503861a5c8de55db16e785613115ce
mbkhalid23 <mbkhalid23@gmail.com>
2023-02-18 22:16:05 +0500

    Hide user roles from registration

--- RailwayReservationSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RailwayReservationSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -201,7 +201,15 @@ namespace RailwayReservationSystem.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "User created successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
