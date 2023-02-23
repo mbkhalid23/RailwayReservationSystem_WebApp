e95092ee657669fb966bea87433f666d0f97e19f
Bilal Khalid <bilal.khalid@teo-intl.com>
2023-02-23 10:42:16 +0500

    Add First API End point

---
+++ RailwayReservationSystem.API/Controllers/TrainAPIController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RailwayReservationSystem.DataAccess.Repository.IRepository;
+using RailwayReservationSystem.Models;
+
+namespace RailwayReservationSystem.API.Controllers
+{
+    [Route("api/TrainAPI")]
+    [ApiController]
+    public class TrainAPIController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TrainAPIController(IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Train>> GetAll()
+        {
+            IEnumerable<Train> trains = _unitOfWork.Train.GetAll().ToList();
+            return Ok(trains);
+        }
+    }
+}
--- RailwayReservationSystem.API/Program.cs
+++ RailwayReservationSystem.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using RailwayReservationSystem.DataAccess.Data;
+using RailwayReservationSystem.DataAccess.Repository.IRepository;
+using RailwayReservationSystem.DataAccess.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("TeoConnection")
+    ));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
