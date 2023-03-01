a41729f99ba82874d3c8e235ccbd21e5788db3b1
Bilal Khalid <bilal.khalid@teo-intl.com>
2023-03-01 15:32:10 +0500

    Add Authentication DTO's

---
+++ RailwayReservationSystem.Models/Dto/LoginResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayReservationSystem.Models.Dto
+{
+    internal class LoginResponseDTO
+    {
+        public LocalUser User { get; set; }
+        public string Token { get; set; }
+
+    }
+}
